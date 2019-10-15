c0d16d4420d2ea2dac99021687612882d78ff90f
Stefan Fruhner <stefan.fruhner@googlemail.com>
2019-10-15 22:02:34 +0200

    Add AndroidPreferencesPreference

---
+++ src/de/firebirdberlin/preference/AndroidPreferencesPreference.java
@@ -0,0 +1,46 @@
+package de.firebirdberlin.preference;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+
+public class AndroidPreferencesPreference extends Preference {
+
+    private static final String prefs = "prefs";
+    public AndroidPreferencesPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        String which = attrs.getAttributeValue(prefs, "which");
+        final Intent intent;
+        switch (which) {
+            case "NotificationListener":
+                if (Build.VERSION.SDK_INT < 18) {
+                    setVisible(false);
+                    setEnabled(false);
+                }
+                intent = new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS);
+                break;
+            case "Sound":
+                intent = new Intent(Settings.ACTION_SOUND_SETTINGS);
+                break;
+            default:
+                throw new IllegalArgumentException("not implemented");
+
+
+        }
+        final Context ctx = context;
+        setOnPreferenceClickListener(
+                new OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        ctx.startActivity(intent);
+                        return true;
+                    }
+                }
+        );
+    }
+}
